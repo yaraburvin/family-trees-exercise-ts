54f878163536983a68b45779c567b6389c8c908f
yarab <yaraburvin@gmail.com>
2022-07-26 12:14:34 +0300

    Stark family tree added

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -2,5 +2,21 @@ import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+    const eddardKids : Person[] = [
+        {name: "Rickon", children: []},
+        {name: "Bran", children: []},
+        {name: "Arya", children: []},
+        {name: "Sansa", children: []},
+        {name: "Robb", children: []},
+        {name: "Jon", children: []}
+    ]
+    const richardKids : Person[]= [
+        {name: "Benjen", children: []},
+        {name: "Lyanna", children: []},
+        {name: "Brandon", children: []},
+        {name: "Eddard", children: eddardKids}
+    ]
+    
+    const topPerson : Person = {name : "Richard", children : richardKids }
+    return topPerson;
 }
